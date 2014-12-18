c3f5eda6477adf655badc41acd7fe7bf2b5c2955
sandeepbazar <sandeepbazar@KVM013-PBXCWL5.mshome.net>
2014-12-18 20:44:46 +0530

    Added login form

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\ant-build\CordovaApp.ap_ \
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\ant-build\classes.dex \
+C:\0.SandeepBazar\1.Work\1.IBM\1.ObjectStorage\1.Apps\2.SwiftStats\2.WorkSpace\2.Dec_8_2014_Netbeans\ObjectStorageStatsApp\platforms\android\CordovaLib\ant-build\classes.jar \
